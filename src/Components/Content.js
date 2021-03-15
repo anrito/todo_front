775f9eb9800261c4dceaa4106f1c7c5d5abec6af
Omari Jalagania <72104525+omarijalagania@users.noreply.github.com>
2021-03-15 21:59:09 +0400

    content change new edit

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SingleContent from './SingleContent';
 
 const Content = () => {
-
+const [completed] = useState(<ion-icon name="checkmark-done-outline" className="completed-icon"></ion-icon>);
+const [notCompleted] = useState(<ion-icon name="alert-circle-outline" className="not-completed-icon"></ion-icon>)
 
     return(
-        <section className="content-section">
-            <div className="recent-tasks">
-                <h3>Recent Tasks (7)</h3>
-                <button className="task-btn">+ New Task</button>
-            </div>
-            <div className="grid-container">
-                <SingleContent />
-                <SingleContent />
-                <SingleContent />
-                <SingleContent />
-                <SingleContent />
-                <SingleContent />
-            </div>
-        </section>
+        <section className="section">
+        <div className="task-cont">
+            <button className="btn">+ New Task</button>
+        </div>
+        <div className="content-cont">
+            <h3>In Progress - 4</h3>
+                <SingleContent notCompleted={notCompleted}/>
+                <SingleContent notCompleted={notCompleted}/>
+                <div className="arrow-down"><ion-icon name="chevron-down-outline"></ion-icon></div>
+            <h3 className="completed">Completed - 3</h3>
+                <SingleContent completed={completed}/>
+        </div>
+    </section>
     );
 }
 
--- src/Components/SingleContent.js
+++ src/Components/SingleContent.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
-const SingleContent = () => {
+const SingleContent = ({notCompleted, completed}) => {
+    const [dropDown, setDropDown] = useState(false);
+
+
+
+    const dropContent = () => {
+        setDropDown(!dropDown);
+    }
+
 
     return(
         <>
-            <div className="grid-item">
-                    <div className="single-content">
-                        <div className="cont-notification">
-                            <div className="notification">Due in 3 days</div>
-                            <div className="dots">...</div>
+            <div className="content">
+                <div className="content-time">
+                    <div className="icon"> {notCompleted} {completed}</div>
+                        <div className="content-title">
+                                <h2>Task Title</h2>
+                                {dropDown && <p className="description">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                    In temporibus sunt odio esse mollitia commodi saepe rerum,
+                                    fugiat veniam. Autem.
+                                </p>}
                         </div>
-                        <div className="header-text">
-                            <h2>Lorem Task Title</h2>
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                                In sapiente blanditiis expedita adipisci voluptatum debitis, 
-                                optio quaerat fuga. Ab quod animi minima ullam corrupti 
-                                fuga sint magni praesentium aliquam optio!
-                            </p>
-                        </div>
-                    </div>
-            </div>
+                            <button onClick={dropContent} className="drop-btn">{dropDown?<ion-icon name="chevron-up-outline"></ion-icon>:<ion-icon name="chevron-down-outline"></ion-icon>}</button>
+                </div>
+            </div> 
         </>
     );
 }
