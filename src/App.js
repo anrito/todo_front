d37049f707f5985284e0fc8c051defcf2b190e97
Omari Jalagania <omari.jalagania@gmail.com>
2021-03-17 22:32:30 +0400

    first api call

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Content from './Components/Content';
 import Header from './Components/Header';
 import Left_bar from './Components/Left_bar';
+import TestLog from './Components/TestLog';
 
 function App() {
+  const [showCont, setShowCont] = useState(false);
+  const [data, setData] = useState({});
+  const [log, setLog] = useState(true);
+  const apiCall = async ({userName, password}) => {
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    const raw = JSON.stringify({
+      "email": userName,
+      "password": password
+    });
+    
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    
+   try {
+    const response = await fetch("https://api-nodejs-todolist.herokuapp.com/user/login", requestOptions)
+    const result = await response.json();
+    console.log(response)
+    setData(result.user)
+    setShowCont(true);
+    setLog(false)
+   } catch (error) {
+       console.log(error);
+   }
+}
+
+
+
   return (
+
     <div>
-      <Header />
+      { log && <TestLog apiCall={apiCall}/>}
+      { showCont && <Header data={data}/> }
       <div class="main_body">
-        <Left_bar />
-        <Content />
+        { showCont && <Left_bar />}
+        {showCont && <Content />}
       </div>
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 
-const Header = () => {
+const Header = ({data}) => {
 
 
     return (
@@ -19,7 +19,7 @@ const Header = () => {
 
 
                 <div className="userImage">
-                    <p>Name Surname test</p>
+                    <p>{data.name}</p>
                     <a href="!#"><img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt="img" /></a>
                     <a href="!#"> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 13l-7 7-7-7m14-8l-7 7-7-7" />
